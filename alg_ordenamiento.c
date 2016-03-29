ce7fa07303a52270bceb3cd22af885343e507790
enio15 <enioprogramacion@gmail.com>
2016-03-28 21:08:26 -0430

    Update alg_ordenamiento.c
    Se añadieron las funciones del método de burbuja

--- alg_ordenamiento.c
+++ alg_ordenamiento.c
@@ -1,1 +1,28 @@
 /*Librería con todos los métodos de ordenamiento*/
+
+void OrdenarMaximo(int *v, int n)
+{
+    int i;
+    int ind, temp;
+    for(i=0; i < n-1; i++) //n-1 pasadas
+    {
+      ind = Maximo(v, n-1-i);
+      temp = v[n-1-i];
+      v[n-1-i] = v[ind];
+      v[ind]=temp;
+      }
+}
+int Maximo(int *v, int n)
+{
+    int i, indMax;
+    int max=-32768; //Mínimo entero
+    for(i=0; i <= n;i++)
+    {
+      if(v[i]>max)
+      {
+        max = v[i];
+        indMax = i;
+      }
+}
+  return indMax;
+}
